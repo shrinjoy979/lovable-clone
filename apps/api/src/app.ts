3788a1cc1e02a61783841ec8f9aa358048784b3f
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-07-05 19:55:51 +0530

    chat skeleton code added

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import type { Request, Response } from "express";
 
+import chatRouter from "./routes/chat.routes.js";
+
 const app = express();
 app.use(express.json());
 
@@ -8,8 +10,8 @@ app.get("/health", (_req: Request, res: Response) => {
     res.json({
         status: 'ok'
     })
-})
-
-app.listen(3001, () => {
-    console.log("Server is runnig on 3001")
 });
+
+app.use("/chat", chatRouter);
+
+export default app;
